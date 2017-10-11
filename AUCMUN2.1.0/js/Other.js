c8b51410129a309fa0da24f301c56f74bb52693c
bobbiegorp <31881372+bobbiegorp@users.noreply.github.com>
2017-10-11 12:13:29 +0200

    Changed nav bar to seperate html with seperate js and css files

--- AUCMUN2.1.0/js/Other.js
+++ AUCMUN2.1.0/js/Other.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-    $(".home").on("click", function() {home()});
-    navBar();
-    
-    nav();
+    $("#nav_bar").load("navbar.html");
     
     moreInfo();
     
@@ -10,39 +7,6 @@ $(document).ready(function () {
         window.open("https://www.facebook.com/profile.php?id=100013721208284&fref=ts");
     });
 });
-
-function navBar() {
-    var menuItem = document.getElementsByClassName("menuItem");
-    $(menuItem).on("mouseenter", function() {
-        if ($("ul.displayList") == undefined) {
-            console.log("alright");
-        }
-        else {$(".displayList").removeClass("showThis")};
-        
-        if (this.nextElementSibling.tagName == 'UL') {
-            this.nextElementSibling.className += " showThis";
-        }
-    })
-    
-    $(menuItem).on("click", function() {
-        if ($("ul.displayList") == undefined) {
-            console.log("alright");
-        } else if ($("ul.displayList").hasClass("showThis")) {
-            $(".displayList").removeClass("showThis"); 
-        } else {
-            this.nextElementSibling.className += " showThis";
-        }
-    })
-    
-    $(".displayList").on("mouseleave", function() {
-        $(".displayList").removeClass("showThis");
-    })
-    
-}
-
-function home() {
-    document.location.href = "index.html";
-};
 
 function moreInfo() {
     $(".teamPic").on("click", function() {
@@ -57,18 +21,6 @@ function moreInfo() {
     });
 }
 
-/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
 
-function nav() {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].onclick = function(){
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        };
-    };   
-};
 
 
---
+++ AUCMUN2.1.0/js/navbar.js
@@ -0,0 +1,55 @@
+﻿// Functions for the navigation bar
+$(document).ready(function () {
+    $(".home").on("click", function () { home() });
+    navBar();
+
+    nav();
+});
+
+function home() {
+    document.location.href = "index.html";
+};
+
+function navBar() {
+    var menuItem = document.getElementsByClassName("menuItem");
+    $(menuItem).on("mouseenter", function () {
+        if ($("ul.displayList") == undefined) {
+            console.log("alright");
+        }
+        else { $(".displayList").removeClass("showThis") };
+
+        if (this.nextElementSibling.tagName == 'UL') {
+            this.nextElementSibling.className += " showThis";
+        }
+    })
+
+    $(menuItem).on("click", function () {
+        if ($("ul.displayList") == undefined) {
+            console.log("alright");
+        } else if ($("ul.displayList").hasClass("showThis")) {
+            $(".displayList").removeClass("showThis");
+        } else {
+            this.nextElementSibling.className += " showThis";
+        }
+    })
+
+    $(".displayList").on("mouseleave", function () {
+        $(".displayList").removeClass("showThis");
+    })
+
+}
+
+/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons.
+The "active" class is used to add a background color to the current button when its belonging panel is open.
+The "show" class is used to open the specific accordion panel */
+function nav() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function () {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        };
+    };
+};
